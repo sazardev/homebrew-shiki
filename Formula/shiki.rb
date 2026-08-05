6214d58233bd277b1b7e73b507a9e4b58d934029
Omar <omar.desarrollo@gmtransporterp.com>
2026-08-05 07:31:09 -0700

    shiki 0.9.0

--- Formula/shiki.rb
+++ Formula/shiki.rb
@@ -1,17 +1,17 @@
 class Shiki < Formula
   desc "TUI note-taking app with a Yazi-inspired three-pane layout and git-backed notebooks"
   homepage "https://github.com/sazardev/shiki"
-  version "0.8.10"
+  version "0.9.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/sazardev/shiki/releases/download/v0.8.10/shiki-v0.8.10-aarch64-apple-darwin.tar.gz"
-      sha256 "945584735f639460f71f70f3013dd4515b34ec3acce363540a99ae8d99d5d645"
+      url "https://github.com/sazardev/shiki/releases/download/v0.9.0/shiki-v0.9.0-aarch64-apple-darwin.tar.gz"
+      sha256 "237b5d605dbd21563d3657de4c0111c71153e6096fff654487c379511d6a695b"
     end
     on_intel do
-      url "https://github.com/sazardev/shiki/releases/download/v0.8.10/shiki-v0.8.10-x86_64-apple-darwin.tar.gz"
-      sha256 "2d725213ffa16bf7ec5f5508331eb53adb7f4e498269f7a902f246275688cc56"
+      url "https://github.com/sazardev/shiki/releases/download/v0.9.0/shiki-v0.9.0-x86_64-apple-darwin.tar.gz"
+      sha256 "28500ed8f9a88f0eacd576266919e7a249961a6404ea807f2f983293e8efc424"
     end
   end
 
